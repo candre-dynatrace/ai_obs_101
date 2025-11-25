c76151df98f364bbc9ab0d7e98d655f0a5910792
Chris Andre - Dynatrace <chris.andre@dynatrace.com>
2025-11-25 19:03:58 +0000

    Removed references to Ollama from the dev container deployment

--- .devcontainer/deployment.sh
+++ .devcontainer/deployment.sh
@@ -6,13 +6,6 @@ kubectl create namespace travel-advisor
 kubectl -n travel-advisor create secret generic pinecone --from-literal=api-key=$PINECONE_API_KEY
 kubectl -n travel-advisor create secret generic dynatrace --from-literal=token=$DT_TOKEN --from-literal=endpoint=$DT_ENDPOINT/api/v2/otlp
 
-# Deploy ollama
-kubectl apply -f deployment/ollama.yaml -n ollama
-echo "Waiting for Ollama to be ready"
-
-# Wait
-kubectl -n ollama wait --for=condition=Ready pod --all --timeout=10m
-
 # Deploy the application
 kubectl apply -f deployment/travel-advisor.yaml -n travel-advisor
 
